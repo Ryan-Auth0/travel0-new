e492e4b6b130f2a96826f5b8e5cabd2ba4501e4f
Ryan-Auth0 <73363667+Ryan-Auth0@users.noreply.github.com>
2020-11-05 22:25:17 -0500

    first commit 7

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,69 @@
+window.addEventListener('load', function() {
+  var webAuth = new auth0.WebAuth({
+    clientID: AUTH0_CLIENT_ID, 
+    domain: AUTH0_DOMAIN,
+    scope: 'openid profile email',
+    responseType: 'token',
+    redirectUri: AUTH0_CALLBACK_URL
+  });
+
+  document.getElementById('btn-login').addEventListener('click', function() {
+    webAuth.authorize();
+  });
+
+  document.getElementById('btn-logout').addEventListener('click', function() {
+    logout();
+  });
+
+  webAuth.parseHash({ hash: window.location.hash }, (err, authResult) => {
+    if (err) {
+      return console.error(err);
+    }
+    if (authResult) {
+      webAuth.client.userInfo(authResult.accessToken, (err, profile) => {
+        if (err) {
+          // Remove expired token (if any)
+          localStorage.removeItem('token');
+          // Remove expired profile (if any)
+          localStorage.removeItem('profile');
+          console.log("remove items");
+          return alert('There was an error getting the profile: ' + err.message);
+        } else {
+          localStorage.setItem('token', authResult.accessToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+          console.log("show tokens/profile");
+          showUserProfile(profile);
+        }
+        window.location.hash = "";
+      });
+    }
+  });
+
+  var checkAuth = function() {
+    var token = localStorage.getItem('token');
+    if (token) {
+      var user_profile = JSON.parse(localStorage.getItem('profile'));
+      showUserProfile(user_profile);
+    } // else: not authorized
+  };
+
+  var showUserProfile = function(profile) {
+    console.log("Full Contact details", profile.user_metadata.fullcontact);
+  };
+
+  var logout = function() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('profile');
+    window.location.href = "/";
+    console.log("logout prompt");
+  };
+
+  checkAuth();
+});
+
+
+
+
 // URL mapping, from hash to a function that responds to that URL action
 const router = {
   "/": () => showContent("content-home"),
