c89d259d9a2df8aaed656d32031f809912a26dd5
Ryan-Auth0 <73363667+Ryan-Auth0@users.noreply.github.com>
2020-11-16 18:51:50 -0500

    test25

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,65 @@
 
+window.addEventListener('load', function() {
+  var webAuth = new auth0.WebAuth({
+    clientID: AUTH0_CLIENT_ID, 
+    domain: AUTH0_DOMAIN,
+    scope: 'openid profile email',
+    responseType: 'token',
+    redirectUri: AUTH0_CALLBACK_URL
+  });
 
+ /* document.getElementById('btn-login').addEventListener('click', function() {
+    webAuth.authorize();
+  }); */
+
+
+  webAuth.parseHash({ hash: window.location.hash }, (err, authResult) => {
+    if (err) {
+      return console.error(err);
+
+    }
+    if (authResult) {
+      webAuth.client.userInfo(authResult.accessToken, (err, profile) => {
+        if (err) {
+          // Remove expired token (if any)
+          localStorage.removeItem('token');
+          // Remove expired profile (if any)
+          localStorage.removeItem('profile');
+          return alert('There was an error getting the profile: ' + err.message);
+        } else {
+          localStorage.setItem('token', authResult.accessToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+          showUserProfile(profile);
+        }
+        window.location.hash = "";
+      });
+    }
+  });
+
+  var checkAuth = function() {
+    var token = localStorage.getItem('token');
+    if (token) {
+      var user_profile = JSON.parse(localStorage.getItem('profile'));
+      showUserProfile(user_profile);
+    } // else: not authorized
+  };
+
+  var showUserProfile = function(profile) {
+    console.log("Full Contact details", profile.user_metadata.fullcontact);
+  };
+
+
+  checkAuth();
+});
+
+
+document.getElementById('qsLoginBtn').addEventListener('click', function() {
+  webAuth.authorize();
+});
+
+document.getElementById('qsLogoutBtn').addEventListener('click', function() {
+  logout();
+});
 
 /**
  * Starts the authentication flow
